ef41e38a7910de4110c4f8604de59e919a47929d
Trench Rat <muslimsahmurzaev@gmail.com>
2024-09-01 12:25:00 +0300

    Initial Commit

---
+++ src/main/java/io/trenchrat/sheepium/SheepiumIndustries.java
@@ -0,0 +1,16 @@
+package io.trenchrat.sheepium;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SheepiumIndustries implements ModInitializer {
+	public static final String MOD_ID = "sheepium-industries";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/io/trenchrat/sheepium/SheepiumIndustriesClient.java
@@ -0,0 +1,10 @@
+package io.trenchrat.sheepium;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SheepiumIndustriesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
